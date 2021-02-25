ee9804635288d823387f325d6480ce1e43e0be4b
Ivan Soskov <awesomefeo_91@mail.ru>
2021-02-25 20:25:53 +0300

    Спринт 4, Ревью 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,62 @@
+// Присвоить значение переменным:
+  // Кнопка открытия попапа
 let showPopupButtonEdit = document.querySelector('.profile__edit-button');
+  // Сам попап
 let popup = document.querySelector('.popup');
+// Кнопка закрытия попапа
 let closePopupButton = document.querySelector('.popup__close');
-
-let formElement = document.querySelector('.formElement');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
-
+// Форма
+let formElement = document.querySelector('.form-edit');
+// Поле ввода имени пользователя
+let nameInput = document.querySelector('.form-edit__input_type_name');
+// Поле ввода рода деятельности пользователя
+let jobInput = document.querySelector('.form-edit__input_type_job');
+// Имя пользователя, отображаемое на главное странице
 let nameProfile = document.querySelector('.profile__full-name');
+// Род деятельности пользователя, отображаемый на главное странице
 let jobProfile = document.querySelector('.profile__profession');
 
-function togglePopupdEdit(event) {
+// Функция открытия попапа
+function addPopupdEdit(event) {
   event.preventDefault();
-  if (popup.classList.contains('popup_opened')) {
-    nameInput.value = "Жак-Ив Кусто";
-    jobInput.value = "Исследователь океана";
-    }
-  popup.classList.toggle('popup_opened');
+  popup.classList.add('popup_opened');
 }
 
-showPopupButtonEdit.addEventListener('click', togglePopupdEdit);
-closePopupButton.addEventListener('click', togglePopupdEdit);
-popup.addEventListener('click', togglePopupdEdit);
+// Функция  закрытия попапа
+function removePopupdEdit(event) {
+  event.preventDefault();
+  // Обновление значений полей ввода из данных профиля с основной страницы
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+   popup.classList.remove('popup_opened');
+}
 
-document.querySelector('.popup__content').addEventListener('click', function (event) {
-  event.stopPropagation();
-});
-
+// Функция обработки ввода данных
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  removePopupdEdit(evt);
 }
+
+// Добавить действие на кнопку:
+  // Открыть попап на кнопку редактирования профиля
+  showPopupButtonEdit.addEventListener('click', addPopupdEdit);
+  // Закрыть попап на кнопку - Крестик
+  closePopupButton.addEventListener('click', removePopupdEdit);
+  // Отправить форму на кнопку "Сохранить" или Enter
   formElement.addEventListener('submit', formSubmitHandler);
 
 
 
+
+
+
+
+
+  // let likeButton = document.querySelector('.element__like');
+  // function addLike (event) {
+  //   event.preventDefault();
+  //   likeButton.classList.add('element__like_active');
+  // }
+  //   likeButton.addEventListener('click', addLike);
